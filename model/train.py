db91572fb2e06cf605a1797dcfa8ce6b7fcb4aeb
AmirHosseinMissMe <amirhossein2324236@gmail.com>
2021-12-10 20:13:49 +0330

    Added basic model loading and dataset loading in train file.

--- model/train.py
+++ model/train.py
@@ -1,6 +1,9 @@
-from curtsies.fmtfuncs import red, green, on_blue, yellow, blue, cyan
 from tokenizers import ByteLevelBPETokenizer
 from transformers import GPT2Config, GPT2LMHeadModel, GPT2Tokenizer
+from datasets import load_dataset
+
+def encode(lines):
+    return tokenizer(lines['text'], add_special_tokens=True, truncation=True, max_length=512)
 
 TRAIN_BASE = False
 TOKENIZER_DIR = "tokenizer"
@@ -33,3 +36,17 @@ tokenizer.add_special_tokens({
 
 t = tokenizer.encode(inp)
 print(t)
+print(tokenizer.decode(t))
+
+config = GPT2Config(
+    vocab_size = tokenizer.vocab_size,
+    bos_token_id = tokenizer.bos_token_id,
+    eos_token_id = tokenizer.eos_token_id
+)
+
+model = GPT2LMHeadModel()
+
+dataset = load_dataset("text", data_files=paths)
+
+dataset.set_transform(encode)
+dataset = dataset['train']
